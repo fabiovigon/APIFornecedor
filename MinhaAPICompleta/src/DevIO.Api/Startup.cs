2d39bea5d39643dea5c03a8b89fa4e5986309b0a
fabiovigon <fabio.goncalves14@fatec.sp.gov.br>
2020-02-17 14:42:20 -0300

    API Fornecedor

---
+++ MinhaAPICompleta/src/DevIO.Api/Startup.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using DevIO.Api.Configuration;
+using DevIO.Data.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+
+namespace DevIO.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MeuDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentityConfiguration(Configuration);
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.WebApiConfig();
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
+            services.ResolveDependencies();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseCors("Development");
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseCors("Production");
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseMvcConfiguration();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/My Api/swagger/v1/swagger.json", "TestService");
+            });
+        }
+    }
+}
---
+++ MinhaAPICompleta/src/DevIO.Api/wwwroot/app/demo-webapi/src/app/user/userService.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { Observable } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { User } from './user';
+import { BaseService } from '../base/baseService';
+
+
+@Injectable()
+export class UserService extends BaseService {
+
+    constructor(private http: HttpClient) { super() }
+
+    protected UrlServiceV1: string = "https://localhost:5001/api/";
+
+    login(user: User): Observable<User> {
+
+        return this.http
+            .post(this.UrlServiceV1 + 'entrar', user, super.ObterHeaderJson())
+            .pipe(
+                map(super.extractData),
+                catchError(super.serviceError)
+            );
+    }
+
+    persistirUserApp(response: any){
+        localStorage.setItem('app.token', response.accessToken);
+        localStorage.setItem('app.user', JSON.stringify(response.userToken));
+    }
+}
